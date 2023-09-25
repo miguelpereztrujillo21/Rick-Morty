339bb08fcd2ea0a6ad8b7232a2546ce61d6b9a29
miguel <miguelpereztrujillo99@gmail.com>
2023-09-25 20:44:40 +0200

    fix errors

--- app/src/main/java/com/example/rickmorty/modules/modules/main/MainActivity.kt
+++ app/src/main/java/com/example/rickmorty/modules/modules/main/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
--- app/src/test/java/com/example/rickmorty/modules/modules/main/MainViewModelTest.kt
+++ app/src/test/java/com/example/rickmorty/modules/modules/main/MainViewModelTest.kt
@@ -5,63 +5,63 @@ import androidx.lifecycle.Observer
 import com.example.rickmorty.modules.api.Api
 import com.example.rickmorty.modules.api.RetrofitHelper
 import com.example.rickmorty.modules.data.models.Character
-import com.example.rickmorty.modules.data.models.Info
 import com.example.rickmorty.modules.data.models.ResponseCharacters
+import io.mockk.Runs
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.every
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.just
 import io.mockk.mockk
-import io.mockk.verify
-import io.mockk.verifySequence
+
+
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.TestCoroutineScope
 import kotlinx.coroutines.test.resetMain
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+
 import retrofit2.Response
 
 @ExperimentalCoroutinesApi
 class MainViewModelTest {
 
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @RelaxedMockK
     private lateinit var viewModel: MainViewModel
-    private var api = RetrofitHelper.getInstance().create(Api::class.java)
+     var api = RetrofitHelper.getInstance().create(Api::class.java)
 
     @Before
     fun setup() {
         api = mockk(relaxed = true)
         viewModel = MainViewModel()
+
     }
 
     @After
     fun tearDown() {
         Dispatchers.resetMain()
     }
+     @Test
+     fun `when the observer receives empylist`() {
+         //Given
+         val observer = mockk<Observer<ArrayList<Character>>>()
+         every { observer.onChanged(any()) }just Runs
+         viewModel.characters.observeForever(observer)
 
-    @get:Rule
-    var rule: InstantTaskExecutorRule = InstantTaskExecutorRule()
+         //When
+         viewModel.characters.value = ArrayList<Character>()
+         //Then
+         coVerify {observer.onChanged(ArrayList<Character>()) }
 
-    @Test
-    fun `test getCharacters success`() {
-        val responseMock = mockk<Response<ResponseCharacters>>(relaxed = true)
-        val charactersMock = mockk<ArrayList<Character>>()
-        val infoMock = mockk<Info>()
-
-        every { responseMock.isSuccessful } returns true
-        every { responseMock.body() } returns ResponseCharacters(charactersMock, infoMock)
-        coEvery { api.getCharacters(any()) } returns responseMock
-
-        val observer = mockk<Observer<List<Character>>>(relaxed = true)
-
-
-
-        viewModel.getCharacters(1)
-    }
+     }
 
     @Test
-    fun `test getCharacters failure server and null list in results`() {
+    suspend fun `test getCharacters failure server and null list in results`() {
 
         val responseMock = mockk<Response<ResponseCharacters>>(relaxed = true)
 
@@ -69,14 +69,14 @@ class MainViewModelTest {
         every { responseMock.code() } returns 500
         every { responseMock.body()?.results } returns  null
         coEvery { api.getCharacters(any()) } returns responseMock
+        coVerify { api.getCharacters(any(), any(), any(), any()) }
 
-
-        viewModel.getCharacters(1)
+        api.getCharacters()
 
         assert(responseMock.body()?.results == null)
 
     }
 
-    // Tests similares a los de getCharacters se pueden escribir para el método getFilteredCharacters
+
 
 }
