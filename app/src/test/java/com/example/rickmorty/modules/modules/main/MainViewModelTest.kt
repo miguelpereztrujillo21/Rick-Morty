33891fd8197449d345e326bbe077907cec8a838b
miguel <miguelpereztrujillo99@gmail.com>
2023-12-30 17:31:39 +0100

    add test and dependencis

--- app/src/test/java/com/example/rickmorty/modules/modules/main/MainViewModelTest.kt
+++ app/src/test/java/com/example/rickmorty/modules/modules/main/MainViewModelTest.kt
@@ -5,20 +5,15 @@ import androidx.lifecycle.Observer
 import com.example.rickmorty.modules.api.Api
 import com.example.rickmorty.modules.api.RetrofitHelper
 import com.example.rickmorty.modules.data.models.Character
-import com.example.rickmorty.modules.data.models.ResponseCharacters
-import io.mockk.Runs
-import io.mockk.coEvery
-import io.mockk.coVerify
-import io.mockk.every
-import io.mockk.impl.annotations.RelaxedMockK
-import io.mockk.just
-import io.mockk.mockk
+
 
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.resetMain
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.After
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -31,49 +26,32 @@ class MainViewModelTest {
     @get:Rule
     val rule = InstantTaskExecutorRule()
 
-    @RelaxedMockK
-    private lateinit var viewModel: MainViewModel
+    lateinit var  viewModel: MainViewModel
+
      var api = RetrofitHelper.getInstance().create(Api::class.java)
 
     @Before
     fun setup() {
-        api = mockk(relaxed = true)
         viewModel = MainViewModel()
 
     }
+
+
 
     @After
     fun tearDown() {
         Dispatchers.resetMain()
     }
      @Test
-     fun `when the observer receives empylist`() {
-         //Given
-         val observer = mockk<Observer<ArrayList<Character>>>()
-         every { observer.onChanged(any()) }just Runs
-         viewModel.characters.observeForever(observer)
+     fun `when the observer receives list the recycler is in screen`() {
+        viewModel.characters.value = ArrayList()
 
-         //When
-         viewModel.characters.value = ArrayList<Character>()
-         //Then
-         coVerify {observer.onChanged(ArrayList<Character>()) }
 
      }
 
     @Test
     suspend fun `test getCharacters failure server and null list in results`() {
 
-        val responseMock = mockk<Response<ResponseCharacters>>(relaxed = true)
-
-        every { responseMock.isSuccessful } returns false
-        every { responseMock.code() } returns 500
-        every { responseMock.body()?.results } returns  null
-        coEvery { api.getCharacters(any()) } returns responseMock
-        coVerify { api.getCharacters(any(), any(), any(), any()) }
-
-        api.getCharacters()
-
-        assert(responseMock.body()?.results == null)
 
     }
 
